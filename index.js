fc8d17926130030cd6e82cd6be8ad5e2c4f50682
HiramZamorano <ahzamorano@gmail.com>
2020-02-13 14:40:44 -0700

    Adds movie details

--- index.js
+++ index.js
@@ -91,5 +91,25 @@ const movieTemplate = (movieDetail) => {
          </div>
         </div>
     </article>
+    <article class="notification is-primary">
+     <p class="title">${movieDetail.Awards}</p>
+     <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+     <p class="title">${movieDetail.BoxOffice}</p>
+     <p class="subtitle">BoxOffice</p>
+    </article>
+    <article class="notification is-primary">
+     <p class="title">${movieDetail.Metascore}</p>
+     <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+     <p class="title">${movieDetail.imdbRating}</p>
+     <p class="subtitle">imdb Rating</p>
+    </article>
+    <article class="notification is-primary">
+     <p class="title">${movieDetail.imdbVotes}</p>
+     <p class="subtitle">imdb Votes</p>
+    </article>
     `;
 };            
