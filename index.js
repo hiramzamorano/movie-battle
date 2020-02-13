4bd785819d8faa6dcb37397ea79b113c47e38f7a
HiramZamorano <ahzamorano@gmail.com>
2020-02-12 19:41:43 -0700

    Fetch movie data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd0812526',
+            s: 'avengers'
+        }
+    });
+    console.log(response.data);
+};
+
+fetchData();
+
